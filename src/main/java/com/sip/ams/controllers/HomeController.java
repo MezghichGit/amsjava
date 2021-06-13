dd96c082a5c4aeaec5a84153116494e71845fd7f
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-06-13 14:26:33 +0100

    add avec post

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,15 +1,29 @@
 package com.sip.ams.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sip.ams.entities.Candidat;
 
 @Controller
 public class HomeController {
+	static ArrayList<Candidat> lc;
+	static {
+		lc = new ArrayList<>();
+		Candidat c1 = new Candidat(1,"Naouel","naouel@gmail.com","11111111");
+		Candidat c2 = new Candidat(2,"Sameh","samed@gmail.com","22222222");
+		Candidat c3 = new Candidat(3,"Amine","amine@gmail.com","33333333");
+		lc.add(c1);
+		lc.add(c2);
+		lc.add(c3);
+	}
 	
 	@RequestMapping("/index")
 	//@ResponseBody
@@ -22,19 +36,17 @@ public class HomeController {
 	@RequestMapping("/candidats")
 	public String listCandidats(Model m)
 	{
-		Candidat c1 = new Candidat(1,"Naouel","naouel@gmail.com","11111111");
-		Candidat c2 = new Candidat(2,"Sameh","samed@gmail.com","22222222");
-		Candidat c3 = new Candidat(3,"Amine","amine@gmail.com","33333333");
+		/*
 		Candidat tab[]=new Candidat[3];
 		tab[0]=c1;
 		tab[1]=c2;
-		tab[2]=c3;
+		tab[2]=c3;*/
 		String libelleFormation ="Spring Boot & Angular";
 		String formateur = "Mohamed Amine Mezghich";
 		
 		m.addAttribute("lf", libelleFormation);
 		m.addAttribute("coach", formateur);
-		m.addAttribute("tab", tab);
+		m.addAttribute("tab",lc);
 		return "home/candidats";
 	}
 	
@@ -43,6 +55,22 @@ public class HomeController {
 	public String addCandidate()
 	{
 		return "home/add";
+	}
+	
+	@PostMapping("/add")
+	//@ResponseBody
+	public String saveCandidate(@RequestParam("id") int id, 
+			                    @RequestParam("nom") String nom,
+			                    @RequestParam("email") String email,
+			                    @RequestParam("tel") String tel)
+	{
+		Candidat temp = new Candidat(id,nom,email,tel);
+		lc.add(temp);
+		
+		//System.out.println(id+" "+nom+" "+email+" "+tel);
+		
+		//return "infos : "+id+" "+nom+" "+email+" "+tel;
+		return "redirect:candidats";
 	}
 
 }
