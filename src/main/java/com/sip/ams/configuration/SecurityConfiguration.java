126cd98ec22fe757743274987c93ca7c251bc4c3
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-06-27 12:31:28 +0100

    security + mail

--- src/main/java/com/sip/ams/configuration/SecurityConfiguration.java
+++ src/main/java/com/sip/ams/configuration/SecurityConfiguration.java
@@ -37,8 +37,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers("/").permitAll() // accès pour toususers
 				.antMatchers("/login").permitAll() // accès pourtous users
 				.antMatchers("/registration").permitAll() // accèspour tous users
+				.antMatchers("/role/**").permitAll()
+				.antMatchers("/accounts/**").permitAll()
 				
-				.antMatchers("/provider/**").hasAuthority("ADMIN").antMatchers("/article/**").hasAuthority("USER")
+				//.antMatchers("/provider/**").hasAuthority("ADMIN").antMatchers("/article/**").hasAuthority("USER")
+				.antMatchers("/provider/**").hasAnyAuthority("ADMIN", "SUPERADMIN")
+				.antMatchers("/article/**").hasAnyAuthority("USER","SUPERADMIN")
+				
 				.anyRequest().authenticated().and().csrf().disable().formLogin() // l'accès defait via un formulaire
 				
 				.loginPage("/login").failureUrl("/login?error=true") // fixer lapage login
